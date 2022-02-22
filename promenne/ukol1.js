2b865cd0afbf76e61eb0f42d900bb2f23110810a
Alenka7 <alena.medkova@gmail.com>
2022-02-22 19:05:44 +0100

    1 file modified

--- promenne/ukol1.js
+++ promenne/ukol1.js
@@ -1,1 +1,12 @@
-console.log( 1 === 1)
+let vek = 15
+
+// Jsi polnoletý
+// Nejsi plnoletý 
+
+// True/False
+if ( vek >= 18) {
+  console.log("Vyborne, jsi plnoletý!")
+} else {
+  console.log("Ne, nejsi plnoletý.")
+}
+
